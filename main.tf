6d73b36ed8292932ffc92e8c2a0a8f33ad0ad5b9
dhkaneda <dennishuynh@protonmail.com>
2022-01-21 09:10:56 -0800

    Preformat and use needs keyword for artifact dependency for terraform:deploy

--- main.tf
+++ main.tf
@@ -1,43 +1,44 @@
 terraform {
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
+}
 
-  # Configure the AWS Provider
-  provider "aws" {
-    region = "us-east-1"
-  }
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 
-  # Create a VPC
-  resource "random_pet" "jar_bucket_name" {
-    prefix = "zoo-tdd"
-    length = 4
-  }
+# Create a VPC
+resource "random_pet" "jar_bucket_name" {
+  prefix = "zoo-tdd"
+  length = 4
+}
 
-  resource "aws_s3_bucket" "jar_bucket" {
-    bucket = random_pet.jar_bucket_name.id
+resource "aws_s3_bucket" "jar_bucket" {
+  bucket = random_pet.jar_bucket_name.id
 
-    acl           = "private"
-    force_destroy = true
-  }
+  acl           = "private"
+  force_destroy = true
+}
 
-  resource "aws_s3_bucket_object" "jar_artifact" {
+resource "aws_s3_bucket_object" "jar_artifact" {
   bucket = aws_s3_bucket.jar_bucket.id
 
   key    = var.BUILD_FILENAME
   source = "build/libs/${var.BUILD_FILENAME}"
 
   etag = filemd5("build/libs/${var.BUILD_FILENAME}")
-  }
+}
 
-  variable "BUILD_FILENAME" {
-    type        = string
-  }
+variable "BUILD_FILENAME" {
+  type = string
+}
 
-  output "BUILD_FILENAME_OUTPUT" {
-    value = var.BUILD_FILENAME
-  }
+output "BUILD_FILENAME_OUTPUT" {
+  value = var.BUILD_FILENAME
+}
+  
